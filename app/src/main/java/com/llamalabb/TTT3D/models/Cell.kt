b8b8773367c205f5d439b912c2b0a13db5e62b41
Brandon John-Freso <brandonjf@gmail.com>
2017-11-04 19:02:37 -0400

    Direction classes

--- app/src/main/java/com/llamalabb/TTT3D/models/Cell.kt
+++ app/src/main/java/com/llamalabb/TTT3D/models/Cell.kt
@@ -3,5 +3,14 @@ package com.llamalabb.TTT3D.models
 /**
  * Created by andy on 11/4/17.
  */
-class Cell {
+data class Cell(val type: CellType? = null) {
+
+    fun scanNeighbors() {
+
+    }
+
+    fun runCheck() {
+
+    }
+
 }
