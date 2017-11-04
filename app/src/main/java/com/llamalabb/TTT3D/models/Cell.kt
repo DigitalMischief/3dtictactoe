4c9e92ab2c7daafd01042f2af7969c62495e8554
Brandon John-Freso <brandonjf@gmail.com>
2017-11-04 19:30:57 -0400

    Added cell classes

--- app/src/main/java/com/llamalabb/TTT3D/models/Cell.kt
+++ app/src/main/java/com/llamalabb/TTT3D/models/Cell.kt
@@ -3,7 +3,7 @@ package com.llamalabb.TTT3D.models
 /**
  * Created by andy on 11/4/17.
  */
-data class Cell(val type: CellType? = null) {
+data class Cell(val position: Position, val type: CellType? = null) {
 
     fun scanNeighbors() {
 
---
+++ app/src/main/java/com/llamalabb/TTT3D/models/Position.kt
@@ -0,0 +1,6 @@
+package com.llamalabb.TTT3D.models
+
+/**
+ * Created by brandon on 11/4/17.
+ */
+data class Position(val X: Int, val Y: Int)
