be5f56b6af86b283134980a4a53ab4e2cabd89be
andygobrien@gmail.com <andygobrien@gmail.com>
2017-11-04 19:32:30 -0400

    Board initializaiton added
    needs to be modified with for cell constructor

--- app/src/main/java/com/llamalabb/TTT3D/models/Board.kt
+++ app/src/main/java/com/llamalabb/TTT3D/models/Board.kt
@@ -5,4 +5,16 @@ package com.llamalabb.TTT3D.models
  */
 data class Board(val boardSize: Int) {
 
+   var board = Array(boardSize, {Array(boardSize, {Cell()})})
+
+
+    fun createBoard(){
+        for(i in 0..board.size -1){
+            var boardRow = Array(boardSize, {Cell()})
+            for(j in 0..boardRow.size -1){
+                boardRow[j] = Cell()
+            }
+            board[i] = boardRow
+        }
+    }
 }
