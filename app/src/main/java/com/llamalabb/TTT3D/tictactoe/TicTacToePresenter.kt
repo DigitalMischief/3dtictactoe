fe93e6470f883c1e2ef6f431457545cce718674f
Brandon John-Freso <brandonjf@gmail.com>
2017-11-06 00:28:42 -0500

    Just adding a test case to the presenter

--- app/src/main/java/com/llamalabb/TTT3D/tictactoe/TicTacToePresenter.kt
+++ app/src/main/java/com/llamalabb/TTT3D/tictactoe/TicTacToePresenter.kt
@@ -1,6 +1,8 @@
 package com.llamalabb.TTT3D.tictactoe
 
+import com.llamalabb.TTT3D.GameManager
 import com.llamalabb.TTT3D.models.Board
+import com.llamalabb.TTT3D.models.Position
 
 /**
  * Created by andy on 11/4/17.
@@ -9,10 +11,23 @@ import com.llamalabb.TTT3D.models.Board
 class TicTacToePresenter(var view: TicTacToeContract.View) : TicTacToeContract.Presenter{
     override fun onStart() {
         createBoard()
+
         view.showBoard(Board())
     }
 
     override fun createBoard() {
 
+
+    }
+
+    private fun runTest() {
+        val testBoard = Board()
+        val manager = GameManager(testBoard)
+        val position = Position(1, 1)
+        val ourCell = testBoard.getCell(position)
+        val ourType = ourCell.type
+        ourType?.let {
+            manager.getAlliesInSearchArea(position, ourType)
+        }
     }
 }
