4379ab8263b95ab5cdc9c8413cf656d38d793add
Brandon John-Freso <brandonjf@gmail.com>
2017-11-05 20:05:11 -0500

    manager now accepts a board

--- app/src/main/java/com/llamalabb/TTT3D/GameManager.kt
+++ app/src/main/java/com/llamalabb/TTT3D/GameManager.kt
@@ -1,18 +1,20 @@
 package com.llamalabb.TTT3D
 
 import android.util.Log
+import com.llamalabb.TTT3D.models.Board
+import com.llamalabb.TTT3D.models.Cell
 import com.llamalabb.TTT3D.models.Position
 
 /**
  * Created by brandon on 11/4/17.
  */
 
-class GameManager() {
+class GameManager(private val board: Board) {
 
     private val searchDistance = 1
     private val TAG = this.javaClass.simpleName
 
-    fun scanNeighbors(position: Position) {
+    fun getNeighbors(position: Position) : List<Cell>{
         val (X, Y) = position
         val startX = X - searchDistance
         val startY = Y - searchDistance
@@ -24,7 +26,10 @@ class GameManager() {
                         .filter { yPos ->
                             !(xPos == X && yPos == Y)
                         }
-                        .forEach { yPos -> Log.d(TAG, "Scanning position - X:$xPos Y:$yPos") }
+                        .forEach { yPos ->
+                            Log.d(TAG, "Scanning position - X:$xPos Y:$yPos")
+                            board.getCell(Position(xPos, yPos))
+                        }
             }
         }
     }
