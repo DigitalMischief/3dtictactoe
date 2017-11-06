031255a7db1cc9fff40c5fe77dc1c81624c92840
Brandon John-Freso <brandonjf@gmail.com>
2017-11-05 19:12:39 -0500

    Adding a game manager class for game logic.

---
+++ app/src/main/java/com/llamalabb/TTT3D/GameManager.kt
@@ -0,0 +1,32 @@
+package com.llamalabb.TTT3D
+
+import android.util.Log
+import com.llamalabb.TTT3D.models.Position
+
+/**
+ * Created by brandon on 11/4/17.
+ */
+
+class GameManager() {
+
+    private val searchDistance = 1
+    private val TAG = this.javaClass.simpleName
+
+    fun scanNeighbors(position: Position) {
+        val (X, Y) = position
+        val startX = X - searchDistance
+        val startY = Y - searchDistance
+        val endX = X + searchDistance
+        val endY = Y + searchDistance
+        if (searchDistance > 0) {
+            for (xPos in maxOf(0, startX)..endX) {
+                (maxOf(0, startY)..endY)
+                        .filter { yPos ->
+                            !(xPos == X && yPos == Y)
+                        }
+                        .forEach { yPos -> Log.d(TAG, "Scanning position - X:$xPos Y:$yPos") }
+            }
+        }
+    }
+
+}
