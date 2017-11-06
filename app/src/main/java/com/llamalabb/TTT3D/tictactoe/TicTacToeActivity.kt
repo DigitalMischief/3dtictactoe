7e72cde4a4168be5605fbbd6c91e04801a0afea0
Brandon John-Freso <brandonjf@gmail.com>
2017-11-05 19:30:09 -0500

    Mods to the activity

--- app/src/main/java/com/llamalabb/TTT3D/tictactoe/TicTacToeActivity.kt
+++ app/src/main/java/com/llamalabb/TTT3D/tictactoe/TicTacToeActivity.kt
@@ -1,13 +1,24 @@
 package com.llamalabb.TTT3D.tictactoe
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.llamalabb.TTT3D.GameManager
 import com.llamalabb.TTT3D.R
+import com.llamalabb.TTT3D.models.Position
 
 class TicTacToeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tic_tac_toe)
+
+        setupGame()
     }
+
+    fun setupGame() {
+        val manager = GameManager()
+        manager.scanNeighbors(Position(4,5))
+    }
+
+
 }
