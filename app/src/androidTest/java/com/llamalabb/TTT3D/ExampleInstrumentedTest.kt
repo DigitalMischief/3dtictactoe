6ae925eff4feda41badf053fe8bb8fdd040c7b79
andygobrien@gmail.com <andygobrien@gmail.com>
2017-11-15 14:18:53 -0500

    minor gradle changes

--- app/src/androidTest/java/com/llamalabb/TTT3D/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/llamalabb/TTT3D/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.llamalabb.a3dtictactoe", appContext.packageName)
+        assertEquals("com.llamalabb.TTT3D", appContext.packageName)
     }
 }
